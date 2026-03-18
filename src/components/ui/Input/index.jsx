051f77cab443760d4e7d5feb945887fd9a412672
BencWislla <beniciowislla@gmail.com>
2026-03-18 13:52:55 +0100

    changing the input style

--- src/components/ui/Input/index.jsx
+++ src/components/ui/Input/index.jsx
@@ -1,19 +1,29 @@
 import { ContainerInput, InputContent, LabelContent } from "./styles";
 
-export function Input({ text, icon, placeholder, value, onChange, alt, type }) {
+export function Input({
+  text,
+  icon,
+  placeholder,
+  value,
+  onChange,
+  alt,
+  type,
+  fontSize,
+}) {
   return (
-    <ContainerInput>
-      {icon}
-      <LabelContent>
-        {text}
+    <LabelContent>
+      {text}
+      <ContainerInput>
+        {icon}
         <InputContent
           placeholder={placeholder}
           value={value}
           alt={alt}
           onChange={onChange}
           type={type}
+          fontSize={fontSize}
         />
-      </LabelContent>
-    </ContainerInput>
+      </ContainerInput>
+    </LabelContent>
   );
 }
--- src/components/ui/Input/styles.js
+++ src/components/ui/Input/styles.js
@@ -4,20 +4,25 @@ export const ContainerInput = styled.div`
 border: 1px solid #D1D5DB;
 border-radius: 8px;
 display: flex;
-flex-direction: ${({ direction }) => direction};
-gap: ${({ gap }) => gap};
-background-color: ${(backgroudColor) => backgroudColor};
+background-color: #ffff;
+align-items: center;
+padding: 12px 9px;
+gap: 12px;
+
 `
 export const InputContent = styled.input`
 border: none;
 outline: none;
-padding: 12px;
-font-size: 1rem;
+font-size: ${(props) => (props.fontSize)};
 line-height: 24px;
+background-color: transparent;
 `
 
 export const LabelContent = styled.label`
 font-size: 14px;
 line-height: 17px;
 color: #374151;
+display: flex;
+flex-direction: column;
+gap: 8px;
 `
