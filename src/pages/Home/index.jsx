80250c36935cefd5f833a12b88aa987996a8aba3
BencWislla <beniciowislla@gmail.com>
2026-03-19 11:46:42 +0100

    Change Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,4 @@
-import { Badge } from "../../components/ui/Badge";
-import { Button } from "../../components/ui/Button";
-import { Input } from "../../components/ui/Input";
-import { Select } from "../../components/ui/Select";
-
 function Home() {
-  return (
-    <div>
-      <Input
-        text={"Nome completo"}
-        placeholder={"Digite seu nome"}
-        type={"password"}
-      />
-      <Select title={"Selecione"} textOne={"Dev"} textTwo={"Senior"} />
-      <Button text={"Confirmar"} />
-      <Badge text={"Inativo"} />
-    </div>
-  );
+  return <div>Home</div>;
 }
 export default Home;
